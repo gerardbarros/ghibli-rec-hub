635bb5834f7c3dc7697bfd57c64700c4f581aca3
Gerard <barros.gc@gmail.com>
2022-04-11 00:53:57 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+document.querySelector("button").addEventListener("click", getMovie)
+
+
+
+function getMovie() {
+  // randomize a number from 0-22
+  let random = Math.floor( Math.random() * 22 )
+  fetch("https://ghibliapi.herokuapp.com/films/")
+    .then(res => res.json())
+    .then(data => {
+      console.log(data[random])
+      // get movie name
+      document.querySelector(".title").innerText = data[random].title
+      // get movie poster image
+      document.querySelector("img").src = data[random].image
+      // // description
+      document.querySelector(".plot").innerText = data[random].description
+      // // release date
+      document.querySelector(".release").innerText = data[random].release_date
+      // // running time
+      document.querySelector(".run-time").innerText = data[random].running_time
+      
+    })
+    .catch(err => {
+        console.log(`error ${err}`)
+    });
+
+}
+
+
