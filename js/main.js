c77303089c4b38ce366593777e7328366d1ec0c2
Gerard <barros.gc@gmail.com>
2022-04-26 01:41:02 -0500

    Updated initial page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ document.querySelector("button").addEventListener("click", getMovie)
 function getMovie() {
   // show movie info
   let movieInfo = document.querySelector("#info")
+  let hideLogo = document.querySelector(".logo")
+  hideLogo.classList.add("hidden")
   movieInfo.classList.remove("hidden")
+  
   // randomize a number from 0-22
   let random = Math.floor( Math.random() * 22 )
   fetch("https://ghibliapi.herokuapp.com/films/")
@@ -13,13 +16,13 @@ function getMovie() {
       // get movie name
       document.querySelector(".title").innerText = data[random].title
       // get movie poster image
-      document.querySelector("img").src = data[random].image
+      document.querySelector(".poster").src = data[random].image
       // // description
       document.querySelector(".plot").innerText = data[random].description
       // // release date
       document.querySelector(".release").innerText = data[random].release_date
       // // running time
-      document.querySelector(".run-time").innerText = data[random].running_time
+      document.querySelector(".run-time").innerText = data[random].running_time + " mins"
       
     })
     .catch(err => {
